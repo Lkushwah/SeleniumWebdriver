dd5bbf2304e43b648d76f0c9db6747919e40efca
laharshk <laharshkushwah@gmail.com>
2024-11-02 00:38:57 +0530

    Selenium First Push

---
+++ src/test/java/Selenium002.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Selenium002 {
+
+
+
+    @Test
+    public void test_001(){
+        FirefoxDriver driver = new FirefoxDriver();
+        driver.get("https://sdet.live");
+        driver.quit();
+
+
+
+
+
+
+    }
+}
